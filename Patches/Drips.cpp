c175c6e7ba572a351c20551300b105ff368de118
FrozenFish24 <xfrozenfishx@gmail.com>
2025-06-12 18:41:56 +0800

    Implement billboarding and configurable droplet size

--- Patches/Drips.cpp
+++ Patches/Drips.cpp
@@ -3,6 +3,7 @@
 #include "Common\Utils.h"
 #include "Logging\Logging.h"
 #include "Patches\Patches.h"
+#include "Common\Settings.h"
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
@@ -13,19 +14,34 @@ IDirect3DDevice8*& g_d3d8Device_A32894 = *reinterpret_cast<IDirect3DDevice8**>(0
 using originalDrawDripsFunc = void (*)(/* UINT primCount */);
 static originalDrawDripsFunc originalDrawDrips = reinterpret_cast< originalDrawDripsFunc>(0x4EDE20);
 
-struct Point {
-    float x;
-    float y;
-    float z;
+struct Vertex {
+    float x, y, z;
     D3DCOLOR diffuse;
 };
 
 struct Line {
-    Point p0;
-    Point p1;
+    Vertex v0, v1;
+};
+
+struct Triangle {
+    Vertex v0, v1, v2;
 };
 
 Line& vertexStreamZeroData_963880 = *reinterpret_cast<Line*>(0x963880);
+
+inline D3DVECTOR normalize(const D3DVECTOR& vec)
+{
+    float magnitude = std::sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+    return { vec.x / magnitude, vec.y / magnitude, vec.z / magnitude };
+}
+
+inline D3DVECTOR cross(const D3DVECTOR& a, const D3DVECTOR& b) {
+    return {
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    };
+}
 
 void drawDrips(UINT primCount)
 {
@@ -64,17 +80,51 @@ void drawDrips(UINT primCount)
         g_d3d8Device_A32894->SetVertexShader(D3DFVF_XYZ | D3DFVF_DIFFUSE);
 
         // NEW CODE
-        //std::vector<Line> lineList;
-        //lineList.assign(&vertexStreamZeroData_963880, &vertexStreamZeroData_963880 + primCount);
+        std::vector<Triangle> triList;
+        const float width = DropletSize;
 
         for (size_t i = 0; i < primCount; i++) {
             Line* line = &(&vertexStreamZeroData_963880)[i];
-            line->p0.diffuse = D3DCOLOR_ARGB(255, 255, 0 ,0);
-            line->p1.diffuse = D3DCOLOR_ARGB(255, 0, 0, 255);
+
+            // TODO: Move to vertex shader
+            const Vertex center = { line->v0.x, (line->v0.y + line->v1.y) / 2, line->v0.z };
+            constexpr D3DVECTOR upVec = {0,1,0};
+
+            const Vertex camera = { GetInGameCameraPosX(), GetInGameCameraPosY(), GetInGameCameraPosZ() };
+            D3DVECTOR toCam = { camera.x - center.x, camera.y - center.y, camera.z - center.z };
+            toCam = normalize(toCam);
+
+            const D3DVECTOR rightVec = normalize(cross(upVec, toCam));
+
+            Vertex v0 = line->v0;
+            Vertex v1 = line->v0;
+            Vertex v2 = line->v1;
+            Vertex v3 = line->v1;
+            
+            v0.x += rightVec.x * (width / 2);
+            v0.y += rightVec.y * (width / 2);
+            v0.z += rightVec.z * (width / 2);
+
+            v1.x -= rightVec.x * (width / 2);
+            v1.y -= rightVec.y * (width / 2);
+            v1.z -= rightVec.z * (width / 2);
+
+            v2.x += rightVec.x * (width / 2);
+            v2.y += rightVec.y * (width / 2);
+            v2.z += rightVec.z * (width / 2);
+
+            v3.x -= rightVec.x * (width / 2);
+            v3.y -= rightVec.y * (width / 2);
+            v3.z -= rightVec.z * (width / 2);
+
+            triList.push_back({ v3, v1, v0 });
+            triList.push_back({ v0, v2, v3 });
         }
+
+        g_d3d8Device_A32894->DrawPrimitiveUP(D3DPT_TRIANGLELIST, triList.size(), triList.data(), 16);
         // END NEW CODE
 
-        g_d3d8Device_A32894->DrawPrimitiveUP(D3DPT_LINELIST, primCount, &vertexStreamZeroData_963880, 16);
+        //g_d3d8Device_A32894->DrawPrimitiveUP(D3DPT_LINELIST, primCount, &vertexStreamZeroData_963880, 16);
 
         g_d3d8Device_A32894->SetRenderState(D3DRS_ZWRITEENABLE, TRUE);
         g_d3d8Device_A32894->SetRenderState(D3DRS_FOGENABLE, TRUE);
